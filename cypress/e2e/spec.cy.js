280f3eda73e3a1d89cc48bd8ec76eb63790e7ea9
John Ammon <ammonj78@gmail.com>
2023-02-08 15:41:25 -0700

    Passed test

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,22 @@
+describe('Main Page', () => {
+    beforeEach(() => {
+      cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies',)
+      cy.visit('http://localhost:3002/')
+    })
+  
+    it('this should pass', () => {
+        cy.get('div')
+          .find('.whereFormWillBe')
+          .should('have.text', 'Find A Movie:')
+    })
+
+    // it('Should see the title of the application', () => {
+    //   cy.get('.logoPic')
+    //     .should('have.attr','src','../images/Tomatillo Logo.png')
+    // })
+
+
+    
+  })
+
+
