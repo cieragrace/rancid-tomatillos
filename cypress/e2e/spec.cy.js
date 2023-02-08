35926e2b56084f4901e22e37dec635f61447e9d2
John Ammon <ammonj78@gmail.com>
2023-02-08 16:01:05 -0700

    passed has a search bar

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -14,6 +14,15 @@ describe('Main Page', () => {
       cy.get('.logoPic')
         .should('have.attr','src','../images/Tomatillo Logo.png')
     })
+    it('Should have a search bar', () => {
+        cy.get('#findMovie').should('be.visible')
+      })
+
+    // it('should have movies', () => {
+    //     cy.get('.movies')
+    //       .find('.movies')
+    //       .should('have.length', 40)
+    // })
 
 
     
