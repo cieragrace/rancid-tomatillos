34c185ad4fb771eb4540e13682a939651d6cc450
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-09 17:00:49 -0700

    add css to files and testing for single view

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -7,8 +7,8 @@ import './Movie.css'
 const Card = ({id, posterPath, backdropPath, title, averageRating, releaseDate, showSingleMovie}) => {
   return (
     <NavLink to={`/${id}`}> 
-      <img className="Card" src={posterPath} alt="Movie Poster" width="383" height="600"></img>
-      <h2 className="homeTitles">{title}</h2>
+      <img className="Card" id={id} src={posterPath} alt="Movie Poster" width="383" height="600"></img>
+      {/* <h2 className="homeTitles">{title}</h2> */}
       {/* <p>{averageRating}</p> */}
       {/* <p>{releaseDate}</p> */}
     </NavLink>
