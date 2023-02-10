69c159cde5cc290d16446f58ee08a5d181cecbb9
John Ammon <ammonj78@gmail.com>
2023-02-09 18:49:34 -0700

    Added NotFound component for error handling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
 import getAPIData from './APICalls.js'
 import Movies from './components/Movies/Movies';
+<<<<<<< Updated upstream
 import Movie from './components/MovieInfo/MovieInfo';
 import { Route, Switch } from 'react-router-dom'
+=======
+import { BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+import Header from './components/Header/Header'
+// import Card from './components/Card/Card';
+>>>>>>> Stashed changes
 import './App.css';
 import Header from './components/Header/Header'
 import NotFound from './components/NotFound.js';
